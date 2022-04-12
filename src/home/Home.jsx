c8d2991d68edaedcb47e8fc6643340d11ec99f23
taras <taraslychynets@icloud.com>
2022-04-12 15:34:44 +0300

    home page

---
+++ src/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.scss'
+
+const Home = () => {
+  return (
+    <div className="page__content">
+      <h1>🏠</h1>
+    </div>
+  );
+}
+
+export default Home
