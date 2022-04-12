9d891e20dc899dc534e2f5a0a55307969afdff3a
taras <taraslychynets@icloud.com>
2022-04-12 15:34:30 +0300

    App

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom"
+import Home from "./home/Home";
+import Todos from "./tasks/components/Todos"
+import Photos from './photos/components/Photos'
+
+import './index.scss'
+
+const App = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="header__home header_link">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="header__todos header_link">
+          <Link to="/todos">Todos</Link>
+        </div>
+        <div className="header__photos header_link">
+          <Link to="/photos">Photos</Link>
+        </div>
+      </header>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="todos" element={<Todos />} />
+        <Route path="photos" element={<Photos />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
