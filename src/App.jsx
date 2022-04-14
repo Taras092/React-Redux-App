beb203d57d758a84292e63f5452b49fd2b4da8d3
taras <taraslychynets@icloud.com>
2022-04-14 20:08:39 +0300

    basic component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./home/Home";
-import Todos from "./tasks/components/Todos"
-import Photos from './photos/components/Photos'
+import Todos from "./tasks/components/Todos";
+import Photos from "./photos/components/Photos";
 
-import './index.scss'
+import "./index.scss";
 
 const App = () => {
   return (
     <>
       <header className="header">
         <div className="header__home header_link">
-          <Link to="/React-Redux-App">Home</Link>
+          <Link to="/">Home</Link>
         </div>
         <div className="header__todos header_link">
-          <Link to="/React-Redux-App/todos">Todos</Link>
+          <Link to="/todos">Todos</Link>
         </div>
         <div className="header__photos header_link">
-          <Link to="/React-Redux-App/photos">Photos</Link>
+          <Link to="/photos">Photos</Link>
         </div>
       </header>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="todos" element={<Todos />} />
-        <Route path="photos" element={<Photos />} />
+          <Route index element={<Home />} />
+          <Route path="todos" element={<Todos />} />
+          <Route path="photos" element={<Photos />} />
       </Routes>
     </>
   );
